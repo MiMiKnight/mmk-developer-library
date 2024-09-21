f7cf7fc361ef0b42c381e7d8b9d04d20380404c8
MiMiKnight <victor2015yhm@gmail.com>
2024-09-21 19:05:50 +0800

    [Requirement Number]US202409201125 [Commit Description]优化

---
+++ kuca/proto-project/kuca-errorcode-processor/src/main/java/cn/mimiknight/developer/kuca/proto/errorcode/processor/GeneralKucaErrorReturnFactory.java
@@ -0,0 +1,19 @@
+package cn.mimiknight.developer.kuca.proto.errorcode.processor;
+
+import cn.mimiknight.developer.kuca.proto.api.errorcode.AbstractKucaErrorReturnFactory;
+import cn.mimiknight.developer.kuca.proto.api.errorcode.model.standard.IKucaErrorReturn;
+
+/**
+ * general kuca error return factory
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @date 2024-09-21 19:03:00
+ */
+public final class GeneralKucaErrorReturnFactory extends AbstractKucaErrorReturnFactory {
+
+    @Override
+    public IKucaErrorReturn getErrorReturn(String code) {
+        // TODO 待完善
+        return null;
+    }
+}
---
+++ kuca/proto-project/kuca-errorcode-processor/src/main/java/cn/mimiknight/developer/kuca/proto/errorcode/processor/GeneralKucaErrorTypeFactory.java
@@ -0,0 +1,19 @@
+package cn.mimiknight.developer.kuca.proto.errorcode.processor;
+
+import cn.mimiknight.developer.kuca.proto.api.errorcode.AbstractKucaErrorTypeFactory;
+import cn.mimiknight.developer.kuca.proto.api.errorcode.model.standard.IKucaErrorType;
+
+/**
+ * general kuca error type factory
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @date 2024-09-21 19:03:39
+ */
+public final class GeneralKucaErrorTypeFactory extends AbstractKucaErrorTypeFactory {
+
+    @Override
+    public IKucaErrorType getErrorType(String id) {
+        // TODO 待完善
+        return null;
+    }
+}
